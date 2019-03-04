280744e2ea573cf98154040da750d8668efbecc5
Puzan <puzansakya@gmail.com>
2019-03-04 20:30:54 +0545

    initial

--- src/app/edit/guards/article-exists.guard.ts
+++ src/app/edit/guards/article-exists.guard.ts
@@ -10,7 +10,8 @@ import {
     switchMap,
     catchError,
     take,
-    filter
+    filter,
+    map
 } from 'rxjs/operators';
 import {
     of,
@@ -19,29 +20,36 @@ import {
 
 // store imports
 import * as fromArticleStore from '../../root-store/article-store';
+import * as fromAuthStore from '../../root-store/auth-store';
+
 import { SeoService } from '../../core/services';
 import { Article } from '../../core/models/article.model';
-import { getArticle } from '../../root-store/article-store';
+import { User } from '../../core/models/user.model';
 
 @Injectable()
 export class ArticleExistsGuard implements CanActivate {
 
     constructor(
         private articleStore: Store<fromArticleStore.ArticleState>,
+        private authStore: Store<fromAuthStore.AuthState>,
         private seo: SeoService
     ) { }
 
     getFromStoreOrAPI(slug: string): Observable<any> {
-        return this.articleStore
-            .select(getArticle).pipe(
-                tap((article: Article) => {                    
-                    if (!article || (article && article.slug !== slug)) {                        
-                        this.articleStore.dispatch(new fromArticleStore.LoadArticle(slug));
-                    }
-                }),
-                filter((data: any) => { return data && data.slug === slug }),
-                take(1)
-            );
+        return this.authStore.select(fromAuthStore.getAuthUser).pipe(
+            tap((user: User) => {
+                if (user) {
+                    this.articleStore.dispatch(new fromArticleStore.LoadArticle(slug));
+                } else {
+                    this.authStore.dispatch(new fromAuthStore.Authenticated);
+                }
+            }),
+            map(() => this.articleStore.select(fromArticleStore.getArticle)), // <-- dispatch loadbookmark action and return articleselectobservable
+            switchMap((article) => article.pipe(
+                take(1),
+                filter((article: Article) => article.user.id === user.id),
+            ))
+        );
     }
 
     canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
